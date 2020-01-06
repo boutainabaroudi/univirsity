baa88bbf4355094be48f19abb76a215230073f49
unknown <boutaina.baroudi@gmail.com>
2020-01-06 11:50:25 +0000

    first commit

---
+++ script/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE student (
+  CNE varchar(20),
+  Nom varchar(30),
+  Prenom varchar(30),
+  Adresse varchar(100) 
+);
+CREATE TABLE module (
+   id int,
+   designation varchar(30)
+   date_debut date
+   date_fin date
+   cer
